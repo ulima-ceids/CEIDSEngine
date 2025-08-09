25538bd3c730703d8eeb4cad2e662781d725c3e0
Hernan Quintana <hernan@jsatch.com>
2025-08-08 21:20:08 -0500

    Agregando libreria SDL

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
+#include <SDL3/SDL.h>
 #include "matematica.h"
 
 // Entrypoint
 int main()
 {
-	std::cout << "Mi primer programa en  C++" << "\n";
-	float resultado = sumar(34.5f, 40.f);
-	std::cout << "El resultado es: " << resultado << "\n";
-	return 0;
+	
 }
